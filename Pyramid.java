99a26dd62b919f691c8a5a50569b0a9a0c18dbc2
Somashekar1996-cpu <somashekarbe1996@gmail.com>
2025-02-06 15:59:56 +0530

    Pyramid

---
+++ Pyramid.java
@@ -0,0 +1,14 @@
+public class Pyramid {
+    public static void main(String[] args) {
+    int N = 5;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N-i; j++) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <=i; k++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
