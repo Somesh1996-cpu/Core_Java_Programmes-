148c1b4fe4c6714417d12dfbcb4549c0e8f6010e
Somashekar1996-cpu <somashekarbe1996@gmail.com>
2025-02-07 00:52:05 +0530

    Left_Half_Pyramid
                     *
                  *  *
               *  *  *
           *  *  *  * 
        *  *  *  *  *

---
+++ Left_Half_Pyramid.java
@@ -0,0 +1,13 @@
+public class Left_Half_Pyramid {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++) {
+            for (int j=1; j<=5-i; j++) {
+                System.out.print("  "); // Firstly, printing spaces from the outer-loop.
+            }
+            for (int k=1; k<=i; k++) {
+                System.out.print("* "); // Printing stars and spaces from the inner-loop.
+            }
+            System.out.println(); // taking curser to the nextline after printing stars and spaces.
+        }
+    }
+}
