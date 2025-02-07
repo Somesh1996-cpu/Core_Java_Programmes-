c240418715e7bb9d9f04b3ec633afcef9776aeaf
Somashekar1996-cpu <somashekarbe1996@gmail.com>
2025-02-07 22:20:21 +0530

    Update Diamond_Pattern.java
    added description.

--- Diamond_Pattern.java
+++ Diamond_Pattern.java
@@ -1,26 +1,36 @@
 public class Diamond_Pattern {
     public static void main(String[] args) {
         int N=5;
-        for (int i=0; i<N; i++) {
+        for (int i=0; i<N; i++) {  // Printing first 5 rows from the outer loop
             for (int j=N-1; j>=i; j--) {
-                System.out.print(" ");
+                System.out.print(" ");  // Printing spaces from the inner-loop
             }
-            for (int k=0; k<=i; k++) {
+            for (int k=0; k<=i; k++) {  // Printing stars from the inner-loop
                 System.out.print("* ");
             }
-            for (int l=N-i; l>=0; l--){
-                System.out.print(" ");
+            System.out.println(); //taking curser to the nextline.
             }
-            System.out.println();
-        }
-        for (int m=0; m<=3; m++) {
+         for (int m=0; m<=3; m++) {   //printing last 4 rows from the outerloop
             for (int o=0; o<=m;o++) {
-                System.out.print(" ");
+                System.out.print(" ");  //printing spaces from the inner-loop
             }
-            for (int p=3; p>=m; p--) {
+            for (int p=3; p>=m; p--) {  //printing stars & spaces from the innerloop 
                 System.out.print(" *");
             }
-            System.out.println();
+            System.out.println(); //taking curser to the nextline.
         }
     }
 }
+
+
+// Output as mentioned below.
+
+     *       
+    * *      
+   * * *     
+  * * * *    
+ * * * * *   
+  * * * *
+   * * *
+    * *
+     *
